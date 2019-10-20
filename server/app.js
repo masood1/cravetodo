61c51c61ec589723c6eb91a7ca3bf986d77807f6
sufimasood <masoodhussain02@gmail.com>
2019-10-20 23:42:10 +0530

    render duntion heck for 500 heruku error

--- server/app.js
+++ server/app.js
@@ -69,11 +69,12 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  // res.render("error");
-  res.json({
-    message: err.message,
-    error: err
-  });
+  console.log("@@@@@@@@@@  error = ", err);
+  res.render("error");
+  // res.json({
+  //   message: err.message,
+  //   error: err
+  // });
 });
 
 module.exports = app;
