95ed777ce0e76720d857044d15c5159ee0f45c49
sufimasood <masoodhussain02@gmail.com>
2019-10-21 16:19:04 +0530

    change in url

--- client/src/common/Constants.js
+++ client/src/common/Constants.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://localhost:9000";
+export const SERVER_URL = "https://cravetodo.herokuapp.com/";
 export const TODOS = SERVER_URL + "/api/v1/todo";
 export const REGISTER = SERVER_URL + "/api/v1/auth/register";
 export const LOGIN = SERVER_URL + "/api/v1/auth/login";
